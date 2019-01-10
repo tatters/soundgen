d38e530708e0dbf4beecb5da26db6a03c0514535
tatters <rty.anik@rambler.ru>
2019-01-10 13:17:40 +0100

    misc

--- R/soundgen.R
+++ R/soundgen.R
@@ -1,4 +1,4 @@
-# TODO: update presets croc + cat (from html); streamline saving all plots a la ggsave: filename, path, different supported devices instead of only png(); automatic addition of pitch jumps at high temp in soundgen() (?)
+# TODO: streamline saving all plots a la ggsave: filename, path, different supported devices instead of only png(); automatic addition of pitch jumps at high temp in soundgen() (?)
 
 #' @import stats graphics utils grDevices
 #' @encoding UTF-8
